9e304a285b786bebb3f7f1cad8f284d70934c980
Ankit Singh <65366532+th3ankit@users.noreply.github.com>
2025-04-06 12:36:58 +0530

    added src/App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to the Space Education ERP App 🚀</h1>
+      <p>This is the frontend setup in React.</p>
+    </div>
+  );
+}
+
+export default App;
