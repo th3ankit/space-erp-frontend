b55203da406c290dc3ca3ff08c75ef66ccc0009a
Ankit Singh <65366532+th3ankit@users.noreply.github.com>
2025-04-07 00:09:08 +0530

    Update ReportsPage.js

--- src/pages/ReportsPage.js
+++ src/pages/ReportsPage.js
@@ -10,7 +10,7 @@ const ReportsPage = () => {
   useEffect(() => {
     const fetchLogs = async () => {
       try {
-        const response = await axios.get(`${process.env.REACT_APP_API_BASE_URL}/api/photos`);
+        const response = await axios.get(`${process.env.REACT_APP_API_BASE_URL}/api/reports`);
         setLogs(response.data);
       } catch (error) {
         console.error('Error fetching reports:', error);
