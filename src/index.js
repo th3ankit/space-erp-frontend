e9d03be0e234feead9f798a7448674f3fdefb995
Ankit Singh <65366532+th3ankit@users.noreply.github.com>
2025-04-06 12:50:57 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+import "./index.css"; // Add this line
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./App.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
