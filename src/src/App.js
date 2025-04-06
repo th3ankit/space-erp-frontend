19fa597903c099e1114f49bbee0cc9b432d72e30
Ankit Singh <65366532+th3ankit@users.noreply.github.com>
2025-04-06 12:47:09 +0530

    Create App.js

---
+++ src/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Space ERP</h1>
+      <p>Frontend is live!</p>
+    </div>
+  );
+}
+
+export default App;
